ce7beb8f880701580c781495ae16ae905f18f288
Jackson Coxson <jkcoxson@gmail.com>
2025-09-05 12:23:49 -0600

    Initial commit for p2

---
+++ p2/p2.cpp
@@ -0,0 +1,22 @@
+// Jackson Coxson
+
+#include <fstream>
+
+struct LetterEntry {
+    char entry;
+    int  number; // this is a u32, so hopefully big enough
+};
+
+// returns false on errors
+bool openFile(std::fstream& input_stream) {
+    return true;
+}
+
+void count() {
+    return;
+}
+
+int main() {
+
+    return 0;
+}
